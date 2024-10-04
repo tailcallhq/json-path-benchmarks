d4e5a2990098a892876ebaa76694109ad351ea12
Sandipsinh Rathod <zotbysandip@gmail.com>
2024-10-04 00:43:24 -0400

    drop unnecessary files

--- benches/root.rs
+++ benches/root.rs
@@ -3,10 +3,6 @@ use criterion::{criterion_group, criterion_main, Criterion};
 mod get_path;
 
 fn all_benchmarks(c: &mut Criterion) {
-    // bench_jsonpath::bench(c);
-    // bench_jsonpath_rust::bench(c);
-    // bench_jsonpath_rs::bench(c);
-    // bench_custom::bench(c);
     get_path::criterion_benchmark(c);
 }
 
